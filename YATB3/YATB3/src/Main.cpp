04f143bacdddf3ec4d736ea2664631e973a80124
yuukieyuukie <toprankedcloser@gmail.com>
2019-03-21 12:46:27 +0900

    最初のコミット（PJ新規）

---
+++ YATB3/YATB3/src/Main.cpp
@@ -0,0 +1,9 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	ChangeWindowMode(true);
+	DxLib_Init();	// DXライブラリ初期化処理
+	WaitKey();	// キー入力待ち
+	DxLib_End();	// DXライブラリ終了処理
+	return 0;
+}
